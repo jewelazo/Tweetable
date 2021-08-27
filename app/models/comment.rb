b9e47759b24a5595d3a4ce55100ab5ebc91f6f1c
jewelazo <gpalominoe@uni.pe>
2021-08-27 17:30:56 +0000

    seed data

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,8 @@
 class Comment < ApplicationRecord
   # Associations
   belongs_to :user
-  belongs_to :tweet
+  belongs_to :tweet, counter_cache: true
   # Validations
   validates :body, presence: true, length: { maximum: 140 }
+  validates :user, uniqueness: { scope: :tweet }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,45 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+Comment.destroy_all
+Tweet.destroy_all
+User.destroy_all
+
+puts "Start seeding Users"
+10.times do
+  user_data = { 
+    email: Faker::Internet.email,
+    username: Faker::Internet.username,
+    name: Faker::Artist.name}
+  User.create(user_data)
+end
+puts "Finish seeding Users"
+
+puts "Start seeding Tweets"
+user_list = User.all
+user_list.each do |user|
+  rand(2..3).times do
+    tweet_data = { 
+      body: Faker::Lorem.paragraph, 
+      user: user }
+    Tweet.create(tweet_data)
+  end
+end
+puts "Finish seeding Tweets"
+
+puts "Start seeding Comments"
+user_list = User.all
+tweet_list = Tweet.all
+
+tweet_list.each do |tweet|
+    user_list.sample(rand(2..4)).each do |user|
+        comment_data = {
+        body: Faker::Lorem.paragraph,
+        user: user, 
+        tweet: tweet }
+        Comment.create(comment_data)
+    end
+end
+puts "Finish seeding Comments"
