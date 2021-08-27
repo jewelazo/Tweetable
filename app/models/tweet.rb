d0381aebed710a355e2a5f71cd81c6d24e3e920b
jewelazo <gpalominoe@uni.pe>
2021-08-27 16:17:15 +0000

    add associations

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,3 +1,4 @@
 class Tweet < ApplicationRecord
   belongs_to :user
+  has_many :comments, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  # Associations
+  has_many :tweets, dependent: :destroy
+  has_many :comments, dependent: :destroy
 end
