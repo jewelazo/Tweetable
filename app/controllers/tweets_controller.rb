5bd48bc3de4dd09413dd1a3f7a6d785a8f55066e
jewelazo <gpalominoe@uni.pe>
2021-08-27 18:12:52 +0000

    add index and show action

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,2 +1,16 @@
 class TweetsController < ApplicationController
+  # GET /tweets
+  def index
+    @tweets = Tweet.all.order(created_at: :desc)
+    @tweet_new = Tweet.new
+  end
+
+  # GET /tweets/:id
+  def show
+    @tweet = Tweet.find_by(id: params[:id])
+    @tweet_comments = @tweet.comments.order(created_at: :desc)
+    # @tweet_comments_new = @tweet_comments.new
+    # @tweet_comments_new.user_id = User.first.id
+    # @tweet_comments_new.category = 'replied_tweet'
+  end
 end
