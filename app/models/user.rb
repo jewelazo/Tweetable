f99713674d2fa5af43e13562f8176a871d56711e
jewelazo <gpalominoe@uni.pe>
2021-08-27 23:36:38 +0000

    add avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   # Associations
   has_many :tweets, dependent: :destroy
   has_many :comments, dependent: :destroy
+  has_one_attached :avatar
   # Validations
   validates :username, :email, presence: true, uniqueness: true
   validates :name, presence: true
